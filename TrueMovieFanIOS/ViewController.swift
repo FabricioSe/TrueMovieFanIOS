2069fe4502cce1ae17e4992962256a02cc6aaedf
Fabricio Segovia <60413944+FabricioSe@users.noreply.github.com>
2023-03-30 13:07:52 -0400

    Initial Commit

---
+++ TrueMovieFanIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrueMovieFanIOS
+//
+//  Created by Fabricio Segovia on 2023-03-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
