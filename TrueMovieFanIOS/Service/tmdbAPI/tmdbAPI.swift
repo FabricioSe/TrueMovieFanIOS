8eadb26c6e6fe1d5a02f91e26bfcdbe74d52f8bd
Fabricio Segovia <60413944+FabricioSe@users.noreply.github.com>
2023-03-31 15:50:21 -0400

    Creating API folder and class for consuming the API

---
+++ TrueMovieFanIOS/Service/tmdbAPI/tmdbAPI.swift
@@ -0,0 +1,15 @@
+//
+//  tmdbAPI.swift
+//  TrueMovieFanIOS
+//
+//  Created by Fabricio Segovia on 2023-03-31.
+//
+
+import Foundation
+import UIKit
+
+class tmdbAPI {
+    static let baseURL = "https://api.themoviedb.org/3/"
+    
+    static func 
+}
